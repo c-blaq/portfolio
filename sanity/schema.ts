403a1f7e67c210f4ca6462ba78b944cfbf9c6e7e
Fathi <babayejuadesina@gmail.com>
2024-01-21 21:43:30 +0100

    update next version

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -1,8 +1,7 @@
 import { type SchemaTypeDefinition } from "sanity";
 
-import blockContent from "./schemas/blockContent";
 import post from "./schemas/post";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [post, blockContent],
+  types: [post],
 };
